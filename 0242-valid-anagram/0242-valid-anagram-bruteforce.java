b23f06dbaf431929275957d45f2f772abdb2151f
Ansh <132825166+Rambo3832@users.noreply.github.com>
2025-03-11 16:10:08 +0530

    Time: 22 ms (10.09%), Space: 44.8 MB (26.46%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram-bruteforce.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length()!=t.length())
+        return false;
+
+        HashMap<Character, Integer> map1= new HashMap<>();
+       
+
+
+        for(int i=0; i<s.length(); i++)
+        {
+            if(map1.containsKey(s.charAt(i)))
+            {
+                int value= map1.get(s.charAt(i));
+
+                map1.replace(s.charAt(i),(value+1));
+            }
+            else
+            map1.put(s.charAt(i),1);
+
+        }
+
+        for(int i=0; i<s.length(); i++)
+        {
+            if(map1.containsKey(t.charAt(i)))
+            {
+                if(map1.get(t.charAt(i))==1)
+                {
+                    map1.remove(t.charAt(i));
+                    
+                }
+                else
+                {
+                int value= map1.get(t.charAt(i));
+
+                map1.replace(t.charAt(i),(value-1));
+                }
+            }
+            
+        }
+        if(map1.isEmpty())
+        return true;
+        else
+        return false;
+    }
+}
